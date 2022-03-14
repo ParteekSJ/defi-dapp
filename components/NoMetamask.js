a10cb0bb5bfe2cb012c4152797c266d4285ce6f6
ParteekSJ <parteeksjamwal@gmail.com>
2022-03-14 17:19:08 +0400

    added rel in <a>

--- components/NoMetamask.js
+++ components/NoMetamask.js
@@ -7,6 +7,7 @@ export default function NoMetamask() {
       <a
         href="https://metamask.io/download"
         target="_blank"
+        rel="noreferrer"
         class="relative inline-block text-lg group">
         <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
           <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
